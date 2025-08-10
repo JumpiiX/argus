3023c2608e30608d747fce5f8c0b2e33030eb266
jumpiix <david@unterguggenberger.ch>
2025-08-11 01:59:50 +0200

    Add CEX price fetcher module for reference price retrieval

---
+++ src/cex/mod.rs
@@ -0,0 +1,28 @@
+/*
+ * CEX price fetcher module for getting reference prices
+ */
+
+mod coinbase;
+mod kraken;
+mod binance;
+
+use async_trait::async_trait;
+use crate::config::CexProvider;
+use crate::models::{CexPrice, Result};
+
+pub use coinbase::CoinbaseClient;
+pub use kraken::KrakenClient;
+pub use binance::BinanceClient;
+
+#[async_trait]
+pub trait CexClient: Send + Sync {
+    async fn get_spot_price(&self, base: &str, quote: &str) -> Result<CexPrice>;
+}
+
+pub fn create_cex_client(provider: &CexProvider) -> Box<dyn CexClient> {
+    match provider {
+        CexProvider::Coinbase => Box::new(CoinbaseClient::new()),
+        CexProvider::Kraken => Box::new(KrakenClient::new()),
+        CexProvider::Binance => Box::new(BinanceClient::new()),
+    }
+}
